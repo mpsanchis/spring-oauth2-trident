6f7a0421fab4585427e1af8db64c62a91bf5c360
Miguel <miguel.perezsanchis@juliusbaer.com>
2022-10-24 12:56:21 +0200

    authorization server

---
+++ spring-oauth2-auth-server/src/test/java/com/mpsanchis/springoauth2authserver/SpringOauth2AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpsanchis.springoauth2authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2AuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
